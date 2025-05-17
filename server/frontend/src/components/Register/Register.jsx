156bdb26a4f604ab81744f94d84f7862dbd62b97
Raquel Lugo-Vecchini <165518342+rlugoven@users.noreply.github.com>
2025-05-16 20:26:04 -0400

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -4,27 +4,19 @@ import user_icon from "../assets/person.png"
 import email_icon from "../assets/email.png"
 import password_icon from "../assets/password.png"
 import close_icon from "../assets/close.png"
-
 const Register = () => {
-// State variables for form inputs
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setlastName] = useState("");
-
-// Redirect to home
   const gohome = ()=> {
     window.location.href = window.location.origin;
   }
-
-// Handle form submission
   const register = async (e) => {
     e.preventDefault();
-
     let register_url = window.location.origin+"/djangoapp/register";
-
-// Send POST request to register endpoint
+    
     const res = await fetch(register_url, {
         method: "POST",
         headers: {
@@ -38,10 +30,8 @@ const Register = () => {
             "email":email
         }),
     });
-
     const json = await res.json();
     if (json.status) {
-    // Save username in session and reload home
         sessionStorage.setItem('username', json.userName);
         window.location.href = window.location.origin;
     }
@@ -50,7 +40,6 @@ const Register = () => {
       window.location.href = window.location.origin;
     }
 };
-
   return(
     <div className="register_container" style={{width: "50%"}}>
       <div className="header" style={{display: "flex",flexDirection: "row", justifyContent: "space-between"}}>
@@ -62,7 +51,6 @@ const Register = () => {
           </div>
           <hr/>
         </div>
-
         <form onSubmit={register}>
         <div className="inputs">
           <div className="input">
@@ -73,22 +61,18 @@ const Register = () => {
             <img src={user_icon} className="img_icon" alt='First Name'/>
             <input type="text"  name="first_name" placeholder="First Name" className="input_field" onChange={(e) => setFirstName(e.target.value)}/>
           </div>
-
           <div>
             <img src={user_icon} className="img_icon" alt='Last Name'/>
             <input type="text"  name="last_name" placeholder="Last Name" className="input_field" onChange={(e) => setlastName(e.target.value)}/>
           </div>
-
           <div>
             <img src={email_icon} className="img_icon" alt='Email'/>
             <input type="email"  name="email" placeholder="email" className="input_field" onChange={(e) => setEmail(e.target.value)}/>
           </div>
-
           <div className="input">
             <img src={password_icon} className="img_icon" alt='password'/>
             <input name="psw" type="password"  placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}/>
           </div>
-
         </div>
         <div className="submit_panel">
           <input className="submit" type="submit" value="Register"/>
@@ -97,5 +81,4 @@ const Register = () => {
       </div>
   )
 }
-
 export default Register;
